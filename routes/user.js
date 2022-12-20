d5d4c90b4286f96d17c3bf1e6f85db336c9b820a
Ali <mail@ali.af>
2022-12-19 19:56:18 -0800

    Bug fixes

--- routes/user.js
+++ routes/user.js
@@ -6,41 +6,44 @@ const db = require('../server/database');
 
 
 // View user profile
-router.get('/:id', (request, response, next) => {
+router.get('/view/:id', (request, response, next) => {
 	const id = request.params.id;
 	const query = `SELECT * FROM users WHERE id = ${id}`;
 	db.query(query, (error, results) => {
-		response.json(results.rows)
-		console.log('here')
+		response.json(results.rows);
+		console.log('here');
 	});
 });
 
 
 // Login user
 router.post('/login', (request, response, next) => {
-	const { email, password } = request.body
+	const { email, password } = request.body;
 	if (email == null || password == null) {
 		response.json({ status: "null-error" });
 	}
-	try {
-		const query = `SELECT * FROM users WHERE email = ${email}`;
-		const data = db.query(query);
-		if (data.rows.length === 0) {
-			response.json({ status: "user-not-found" });
-		}
-		const user = data.rows[0];
-		const matches = bcrypt.compareSync(password, user.password);
-		if (!matches) { response.json({ status: "password-error" }); }
-		request.session.user = {
-			id: user.id,
-			email: user.email
-		};
-		response.json({ user: request.session.user });
-	}
-	catch (error) {
-		console.error(error);
-        response.json({ status: "error" });
-	}
+	const query = `SELECT * FROM users WHERE email = '${email}'`;
+	db.query(query)
+		.then(users => {
+			const user = users.rows[0];
+			console.log(users.rows);
+			if (users.rows.length === 0) { response.json({ status: "user-not-found" }); }
+			else {
+				const matches = bcrypt.compareSync(password, user.password);
+				if (!matches) { response.json({ status: "password-error" }); }
+				else {
+					request.session.user = {
+						id: user.id,
+						email: user.email
+					};
+					response.json({ user: request.session.user });
+				}
+			}
+		})
+		.catch (error => {
+			console.error(error.stack);
+	        response.json({ status: "db-error" });
+		});
 });
 
 
@@ -59,22 +62,29 @@ router.post('/logout', async(request, response) => {
 
 // Register new user
 router.post('/register', (request, response, next) => {
-	// TODO: Verify email doesn't already exist and password meets requirements
-	const { email, firstname, lastname, password } = request.body
+	// TODO: Verify password requirements
+	const { email, firstname, lastname, password } = request.body;
+	const passwordHashed = bcrypt.hashSync(password, 10);
+	const query_check = `SELECT * FROM users WHERE email = '${email}'`
+	const query_insert = `INSERT INTO users(email, firstname, lastname, password) VALUES('${email}', '${firstname}', '${lastname}', '${passwordHashed}') RETURNING *`;
 	if (email == null || firstname == null || lastname == null || password == null) {
 		response.json({ status: "null-error" });
 	}
-	const passwordHashed = bcrypt.hashSync(password, 10);
-	console.log(passwordHashed);
-	const query = `INSERT INTO users(email, firstname, lastname, password) VALUES('${email}', '${firstname}', '${lastname}', '${passwordHashed}') RETURNING *`;
-	db.query(query)
+	db.query(query_check)
 		.then(users => {
-			const user = users.rows[0];
-			request.session.user = {
-	            id: user.id,
-	            email: user.email
-	        };
-			response.json({ status: "ok" });
+			if (users.rows.length > 0) { response.json({ status: "email-error" }); }
+			else {
+				db.query(query_insert)
+					.then(users => {
+						const user = users.rows[0];
+						request.session.user = {
+				            id: user.id,
+				            email: user.email
+				        };
+						console.log(request.session.user);
+						response.json({ status: "ok" });
+					});
+			}
 		})
 		.catch(error => {
 			console.error(error.stack);
@@ -84,10 +94,10 @@ router.post('/register', (request, response, next) => {
 
 
 // Update user account
-router.put('/:id', (request, response, next) => {
+router.put('/update/:id', (request, response, next) => {
 	// TODO: Verify email doesn't already exist and password meets requirements
-	const { email, firstname, lastname, password } = request.body
-
+	const { email, firstname, lastname, password } = request.body;
+	//
 });
 
 
